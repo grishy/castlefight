fe7222b7f01f73afc55610def620dd88c36ac08a
Grishy <grishy.ru@yandex.ru>
2018-07-27 16:59:16 +0300

    ws on client

--- client/src/main.ts
+++ client/src/main.ts
@@ -7,12 +7,38 @@ class App {
     private scene: Scene;
     private canvas: HTMLCanvasElement;
     private ctx: CanvasRenderingContext2D;
+    private socket: WebSocket;
 
     constructor() {
         this.canvas = <HTMLCanvasElement>document.getElementById("game");
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
         this.ctx = this.canvas.getContext("2d");
+
+        // Create WebSocket connection.
+        this.socket = new WebSocket("ws://localhost:9999");
+
+        this.socket.onopen = function() {
+            console.log("Соединение установлено.");
+        };
+
+        this.socket.onclose = function(event) {
+            if (event.wasClean) {
+                console.log("Соединение закрыто чисто");
+            } else {
+                console.log("Обрыв соединения"); // например, "убит" процесс сервера
+            }
+            console.log("Код: " + event.code + " причина: " + event.reason);
+        };
+
+        this.socket.onmessage = event => {
+            console.log("Получены данные " + event.data);
+            this.socket.send("Hello Server!");
+        };
+
+        this.socket.onerror = function(error) {
+            console.log("Ошибка " + error);
+        };
     }
 
     public setup(): void {
