d359c4736c3655de8fc84268638e990d8dff61f1
Grishy <mailgrishy@gmail.com>
2019-01-06 20:00:48 +0300

    fix #10 mouse control

--- client/src/wc3Control.js
+++ client/src/wc3Control.js
@@ -10,14 +10,29 @@ function rotateAroundWorldAxis(obj, axis, radians) {
   obj.setRotationFromMatrix(obj.matrix);
 }
 
+// The four arrow keys
+const KEYS = {
+  LEFT: 37, UP: 38, RIGHT: 39, BOTTOM: 40,
+};
+
+const OFFSET = 0.03; // %
+const SPEED_KEY = 1;
+const SPEED_MOUSE = 0.5;
 
 class Wc3Control {
   constructor(camera) {
     this.camera = camera;
 
+    // TODO: Изменить на более простое изменение угла в моровом пространстве.
     rotateAroundWorldAxis(this.camera, new THREE.Vector3(1, 0, 0), -55 * THREE.Math.DEG2RAD);
     rotateAroundWorldAxis(this.camera, new THREE.Vector3(0, 1, 0), -90 * THREE.Math.DEG2RAD);
     rotateAroundWorldAxis(this.camera, new THREE.Vector3(0, 0, 1), 0);
+
+    this.mouseX = undefined;
+    this.mouseY = undefined;
+
+    window.addEventListener('keydown', this.onKeyDown.bind(this), false);
+    window.addEventListener('mousemove', this.onMouseMove.bind(this), false);
   }
 
   camera() {
@@ -25,9 +40,49 @@ class Wc3Control {
   }
 
   update() {
-    // this.camera.position.z += 0.01;
-    // this.camera.rotation.y += 0.01;
-    console.log(this.camera.rotation.y);
+    const widthOffset = window.innerWidth * OFFSET;
+    const heightOffset = window.innerHeight * OFFSET;
+
+    if (this.mouseX <= widthOffset) {
+      this.camera.position.z -= SPEED_MOUSE;
+    } else if (this.mouseX >= window.innerWidth - widthOffset) {
+      this.camera.position.z += SPEED_MOUSE;
+    }
+
+    if (this.mouseY <= heightOffset) {
+      this.camera.position.x += SPEED_MOUSE;
+    } else if (this.mouseY >= window.innerHeight - heightOffset) {
+      this.camera.position.x -= SPEED_MOUSE;
+    }
+  }
+
+  onKeyDown(event) {
+    // console.log('handleKeyDown', event);
+
+    switch (event.keyCode) {
+      case KEYS.UP:
+        this.camera.position.x += SPEED_KEY;
+        break;
+
+      case KEYS.BOTTOM:
+        this.camera.position.x -= SPEED_KEY;
+        break;
+
+      case KEYS.LEFT:
+        this.camera.position.z -= SPEED_KEY;
+        break;
+
+      case KEYS.RIGHT:
+        this.camera.position.z += SPEED_KEY;
+        break;
+    }
+  }
+
+  onMouseMove(event) {
+    // console.log('handleMouseMove', event);
+
+    this.mouseX = event.x;
+    this.mouseY = event.y;
   }
 }
 
