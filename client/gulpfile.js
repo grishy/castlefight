90dc52b572ea4dedc7f617bfaddd412c9eb4ef89
Grishy <mailgrishy@gmail.com>
2019-01-06 17:30:37 +0300

    gulp auto-reload

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,49 +1,52 @@
-const { src, dest, parallel, watch } = require("gulp");
+const {
+  src, dest, parallel, watch,
+} = require('gulp');
 
-const connect = require("gulp-connect");
-const sourcemaps = require("gulp-sourcemaps");
-const rollup = require("gulp-better-rollup");
+const connect = require('gulp-connect');
+const sourcemaps = require('gulp-sourcemaps');
+const rollup = require('gulp-better-rollup');
 
-function public() {
-  return src("public/**/*")
-    .pipe(dest("dist"))
+function publicFile() {
+  return src('public/**/*')
+    .pipe(dest('dist'))
     .pipe(connect.reload());
 }
 
 function vendor() {
-  return src("vendor/**/*")
-    .pipe(dest("dist/vendor"))
+  return src('vendor/**/*')
+    .pipe(dest('dist/vendor'))
     .pipe(connect.reload());
 }
 
 function js() {
-  return src("src/main.js")
+  return src('src/main.js')
     .pipe(sourcemaps.init())
     .pipe(
       rollup(
         {},
         {
-          format: "umd"
-        }
-      )
+          format: 'umd',
+        },
+      ),
     )
     .pipe(sourcemaps.write())
-    .pipe(dest("dist"))
+    .pipe(dest('dist'))
+    .pipe(connect.reload());
 }
 
 function localServer() {
   return connect.server({
-    root: "dist",
+    root: 'dist',
     port: 8888,
-    livereload: true
+    livereload: true,
   });
 }
 
 function livereload() {
-  watch("src/**/*.*", parallel(js));
-  watch("public/**/*.*", parallel(public));
-  watch("vendor/**/*.*", parallel(vendor));
+  watch('src/**/*.*', parallel(js));
+  watch('public/**/*.*', parallel(publicFile));
+  watch('vendor/**/*.*', parallel(vendor));
 }
 
-exports.build = parallel(public, livereload, js, vendor);
-exports.default = parallel(exports.build, localServer);
+exports.build = parallel(publicFile, js, vendor);
+exports.default = parallel(exports.build, livereload, localServer);
--- client/src/main.js
+++ client/src/main.js
@@ -1,26 +1,28 @@
-var container, stats, controls;
-			var camera, scene, renderer, light;
-			init();
-			animate();
-			function init() {
-				container = document.createElement( 'div' );
-				document.body.appendChild( container );
-				camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 1000 );
-				camera.position.set( - 1.8, 0.9, 2.7 );
-				controls = new THREE.OrbitControls( camera );
-				controls.target.set( 0, - 0.2, - 0.2 );
-				controls.update();
-				var urls = [ 'posx.jpg', 'negx.jpg', 'posy.jpg', 'negy.jpg', 'posz.jpg', 'negz.jpg' ];
-				var loader = new THREE.CubeTextureLoader().setPath( 'textures/' );
-				var background = loader.load( urls );
-				scene = new THREE.Scene();
-				scene.background = background;
-				light = new THREE.HemisphereLight( 0xbbbbff, 0x444422 );
-				light.position.set( 0, 1, 0 );
-				scene.add( light );
-				// model
-				var loader = new THREE.GLTFLoader().setPath( 'models/Skull/' );
-				loader.load( 'scene.gltf', function ( gltf ) {
+let container; var stats; var 
+controls;
+let camera; var scene; var renderer; var 
+light;
+init();
+animate();
+function init() {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 1000);
+  camera.position.set(-1.8, 0.9, 2.7);
+  controls = new THREE.OrbitControls(camera);
+  controls.target.set(0, -0.2, -0.2);
+  controls.update();
+  let urls = ['posx.jpg', 'negx.jpg', 'posy.jpg', 'negy.jpg', 'posz.jpg', 'negz.jpg'];
+  var loader = new THREE.CubeTextureLoader().setPath('textures/');
+  let background = loader.load(urls);
+  scene = new THREE.Scene();
+  scene.background = background;
+  light = new THREE.HemisphereLight(0xbbbbff, 0x444422);
+  light.position.set(0, 1, 0);
+  scene.add(light);
+  // model
+  var loader = new THREE.GLTFLoader().setPath('models/Skull/');
+  loader.load('scene.gltf', ( gltf ) => {
 					gltf.scene.traverse( function ( child ) {
 						if ( child.isMesh ) {
 							child.material.envMap = background;
@@ -28,28 +30,28 @@ var container, stats, controls;
           } );
           gltf.scene.position.y=-25;
 					scene.add( gltf.scene );
-				}, undefined, function ( e ) {
+				}, undefined, ( e ) => {
 					console.error( e );
-				} );
-				renderer = new THREE.WebGLRenderer( { antialias: true } );
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				renderer.gammaOutput = true;
-				container.appendChild( renderer.domElement );
-				window.addEventListener( 'resize', onWindowResize, false );
-				// stats
-				stats = new Stats();
-				container.appendChild( stats.dom );
-			}
-			function onWindowResize() {
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
-				renderer.setSize( window.innerWidth, window.innerHeight );
-			}
-			//
-			function animate() {
-				requestAnimationFrame( animate );
-				renderer.render( scene, camera );
-        stats.update();
-        camera.position.z+=0.1
-			}
+				});
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.gammaOutput = true;
+  container.appendChild(renderer.domElement);
+  window.addEventListener('resize', onWindowResize, false);
+  // stats
+  stats = new Stats();
+  container.appendChild(stats.dom);
+}
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+//
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  stats.update();
+  camera.position.z += 0.1;
+}
