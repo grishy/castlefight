a316ad9b35b662314dfaf13bfc25493b943439fb
Grishy <mailgrishy@gmail.com>
2019-05-11 16:31:15 +0300

    Model

--- client/src/main.js
+++ client/src/main.js
@@ -6,18 +6,25 @@ window.addEventListener('DOMContentLoaded', () => {
   const canvas = document.getElementById('renderCanvas');
   const engine = new BABYLON.Engine(canvas, true);
 
-  const createScene = function () {
-    const scene = new BABYLON.Scene(engine);
-    const camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
-    camera.setTarget(BABYLON.Vector3.Zero());
+  var createScene = function () {
+    var scene = new BABYLON.Scene(engine);
+
+    //Adding a light
+    var light = new BABYLON.HemisphericLight();
+
+    //Adding an Arc Rotate Camera
+    var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0.8, 10, BABYLON.Vector3.Zero(), scene);
     camera.attachControl(canvas, false);
-    const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
-    const sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
-    sphere.position.y = 1;
-    const ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
+
+    // The first parameter can be used to specify which mesh to import. Here we import all meshes
+    BABYLON.SceneLoader.Append("https://models.babylonjs.com/", "Alien.glb", scene, function (newMeshes) {
+      scene.activeCamera = null;
+      scene.createDefaultCameraOrLight(true);
+      scene.activeCamera.attachControl(canvas, false);
+    });
 
     return scene;
-  };
+  }
 
   const scene = createScene();
 
