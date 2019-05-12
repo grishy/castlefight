d1ae7fbee5756b6943db585b2f295307dc62cca1
Grishy <mailgrishy@gmail.com>
2019-05-12 12:02:15 +0300

    Fix build minify

--- client/config/webpack.prod.js
+++ client/config/webpack.prod.js
@@ -1,6 +1,7 @@
 const WebpackMerge = require('webpack-merge');
 const common = require('./webpack.common');
-const UglifyJSWebpackPlugin = require('uglifyjs-webpack-plugin');
+
+const TerserPlugin = require('terser-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
@@ -8,16 +9,11 @@ module.exports = WebpackMerge(common, {
 	mode: 'production',
 	optimization: {
 		minimizer: [
-			new UglifyJSWebpackPlugin({
-				uglifyOptions: {
-					ie8: false,
-					compress: true,
-					mangle: true,
-					warnings: false,
-					parallel: true,
-					cache: true,
-					sourceMap: true
-				}
+			new TerserPlugin({
+				parallel: true,
+				terserOptions: {
+					ecma: 6,
+				},
 			}),
 			new OptimizeCSSAssetsPlugin({})
 		]
